e6b8032a7a6851d3f702c0db5bfa9d4bc894cb0c
Dustin Mikusko <dmikusko@fixdrepair.com>
2020-09-24 14:57:18 -0600

    Clean up puppeteer methods

--- ng-bev-server/Puppeteer/portfolio.js
+++ ng-bev-server/Puppeteer/portfolio.js
@@ -1,78 +1,189 @@
-const puppeteer = require('puppeteer');
+const { response } = require("express");
+const puppeteer = require("puppeteer");
 
-export const generatePortfolioTable = async (fund) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`https://www.morningstar.com/funds/xnas/${fund}/portfolio`);
-  const yes = await page.$('#__layout > div > div.mdc-page-shell__content.mds-page-shell__content > main > div.fund__content > div > div > div.mdc-column.mds-layout-grid__col.fund__content-sal.mds-layout-grid__col--12.mds-layout-grid__col--auto-at-1000 > sal-components > section > div > div > div > div > div:nth-child(2) > div.sal-small-12.sal-columns > div.sal-row.ng-scope > div:nth-child(2) > div > div > div:nth-child(3) > div > div > div:nth-child(2) > div > div.sal-component-body.ng-scope > div > div > table > tbody')
-  console.log(yes);
-  if (yes) {
-    console.log('yes')
-  } else {
-    console.log('hello')
-  }
+exports.methods = {
+  generatePortfolioTable: async (funds) => {
+    const stockTable = [];
 
-  // await page.waitForSelector('#__layout > div > div.mdc-page-shell__content.mds-page-shell__content > main > div.fund__content > div > div > div.mdc-column.mds-layout-grid__col.fund__content-sal.mds-layout-grid__col--12.mds-layout-grid__col--auto-at-1000 > sal-components > section > div > div > div > div > div:nth-child(2) > div.sal-small-12.sal-columns > div.sal-row.ng-scope > div:nth-child(2) > div > div > div:nth-child(3) > div > div > div:nth-child(2) > div > div.sal-component-body.ng-scope > div > div > table > tbody');
+    for (let i = 0; i < funds.length; i++) {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      try {
+        page.on("response", async (resp) => {
+          const request = resp.request();
+          if (request.url().includes("sal-service/v1/fund/process/stockStyle")) {
+            const stringText = await resp.text();
+            const text = JSON.parse(stringText);
+            const stock = {
+              [fund]: {
+                    "Price/Earnings": {
+                      "Fund": text.fund.prospectiveEarningsYield.toFixed(2),
+                      "Cat. Average": text.categoryAverage.prospectiveEarningsYield.toFixed(2),
+                      "Index": text.indexAverage.prospectiveEarningsYield.toFixed(2),
+                    },
+                    "Price/Book": {
+                      "Fund": text.fund.prospectiveBookValueYield.toFixed(2),
+                      "Cat. Average": text.categoryAverage.prospectiveBookValueYield.toFixed(2),
+                      "Index": text.indexAverage.prospectiveBookValueYield.toFixed(2),
+                    },
+                    "Price/Sales": {
+                      "Fund": text.fund.prospectiveRevenueYield.toFixed(2),
+                      "Cat. Average": text.categoryAverage.prospectiveRevenueYield.toFixed(2),
+                      "Index": text.indexAverage.prospectiveRevenueYield.toFixed(2),
+                    },
+                    "Price/Cash Flow": {
+                      "Fund": text.fund.prospectiveCashFlowYield.toFixed(2),
+                      "Cat. Average": text.categoryAverage.prospectiveCashFlowYield.toFixed(2),
+                      "Index": text.indexAverage.prospectiveCashFlowYield.toFixed(2),
+                    },
+                    "Dividend Yield %'": {
+                      "Fund": text.fund.prospectiveDividendYield.toFixed(2),
+                      "Cat. Average": text.categoryAverage.prospectiveDividendYield.toFixed(2),
+                      "Index": text.indexAverage.prospectiveDividendYield.toFixed(2),
+                    },
+                    "Long-Term Earnings %": {
+                      "Fund": text.fund.forecasted5YearEarningsGrowth.toFixed(2),
+                      "Cat. Average": text.categoryAverage.forecasted5YearEarningsGrowth.toFixed(2),
+                      "Index": text.indexAverage.forecasted5YearEarningsGrowth.toFixed(2),
+                    },
+                    "Historical Earnings %": {
+                      "Fund": text.fund.forecastedEarningsGrowth.toFixed(2),
+                      "Cat. Average": text.categoryAverage.forecastedEarningsGrowth.toFixed(2),
+                      "Index": text.indexAverage.forecastedEarningsGrowth.toFixed(2),
+                    },
+                    "Sales Growth %": {
+                      "Fund": text.fund.forecastedRevenueGrowth.toFixed(2),
+                      "Cat. Average": text.categoryAverage.forecastedRevenueGrowth.toFixed(2),
+                      "Index": text.indexAverage.forecastedRevenueGrowth.toFixed(2),
+                    },
+                    "Cash-Flow Growth %": {
+                      "Fund": text.fund.forecastedCashFlowGrowth.toFixed(2),
+                      "Cat. Average": text.categoryAverage.forecastedCashFlowGrowth.toFixed(2),
+                      "Index": text.indexAverage.forecastedCashFlowGrowth.toFixed(2),
+                    },
+                    "Book-Value Growth %": {
+                      "Fund": text.fund.forecastedBookValueGrowth.toFixed(2),
+                      "Cat. Average": text.categoryAverage.forecastedBookValueGrowth.toFixed(2),
+                      "Index": text.indexAverage.forecastedBookValueGrowth.toFixed(2),
+                    },
+                  }
+              };
+              stockTable.push(stock);
+          }
+        });
+        await page.goto(
+          `https://www.morningstar.com/funds/xnas/${funds[i]}/portfolio`
+        );
+        await page.waitForTimeout(2500);
+        await browser.close();
+      } catch {
+        console.log(`Error getting ${funds[i]}`);
+        await browser.close();
 
-  const table = await page.$$eval('#__layout > div > div.mdc-page-shell__content.mds-page-shell__content > main > div.fund__content > div > div > div.mdc-column.mds-layout-grid__col.fund__content-sal.mds-layout-grid__col--12.mds-layout-grid__col--auto-at-1000 > sal-components > section > div > div > div > div > div:nth-child(2) > div.sal-small-12.sal-columns > div.sal-row.ng-scope > div:nth-child(2) > div > div > div:nth-child(3) > div > div > div:nth-child(2) > div > div.sal-component-body.ng-scope > div > div > table > tbody > tr', rows => rows.map(row => row.innerText));
-
-  const spaced = table.map(row => row.split('\t'))
-
-  const stock = {
-    [fund]: {
-      "Price/Earnings": {
-        "Fund": spaced[0][1],
-        "Cat. Average": spaced[0][2],
-        "Index": spaced[0][3]
-      },
-      "Price/Book": {
-        "Fund": spaced[1][1],
-        "Cat. Average": spaced[1][2],
-        "Index": spaced[1][3]
-      },
-      "Price/Sales": {
-        "Fund": spaced[2][1],
-        "Cat. Average": spaced[2][2],
-        "Index": spaced[2][3]
-      },
-      "Price/Cash Flow": {
-        "Fund": spaced[3][1],
-        "Cat. Average": spaced[3][2],
-        "Index": spaced[3][3]
-      },
-      "Dividend Yield %'": {
-        "Fund": spaced[4][1],
-        "Cat. Average": spaced[4][2],
-        "Index": spaced[4][3]
-      },
-      "Long-Term Earnings %": {
-        "Fund": spaced[5][1],
-        "Cat. Average": spaced[5][2],
-        "Index": spaced[5][3]
-      },
-      "Historical Earnings %": {
-        "Fund": spaced[6][1],
-        "Cat. Average": spaced[6][2],
-        "Index": spaced[6][3]
-      },
-      "Sales Growth %": {
-        "Fund": spaced[7][1],
-        "Cat. Average": spaced[7][2],
-        "Index": spaced[7][3]
-      },
-      "Cash-Flow Growth %": {
-        "Fund": spaced[8][1],
-        "Cat. Average": spaced[8][2],
-        "Index": spaced[8][3]
-      },
-      "Book-Value Growth %": {
-        "Fund": spaced[9][1],
-        "Cat. Average": spaced[9][2],
-        "Index": spaced[9][3]
-      },
+      }
     }
-  };
-  await browser.close();
+    return stockTable;
+  },
+};
 
-  console.log(stock)
-}
+
+
+// const stocks = [
+//   "etilx",
+//   "etihx",
+//   "etimx",
+//   "etidx",
+//   "etibx",
+//   "pksfx",
+//   "pxsgx",
+//   "viisx",
+//   "ggezx",
+//   "ambfx",
+//   "amefx",
+//   "gvtfx",
+// ];
+
+// testPortfolio = async (funds) => {
+//   const stockTable = [];
+
+//     for (let i = 0; i < funds.length; i++) {
+//       const browser = await puppeteer.launch();
+//       const page = await browser.newPage();
+//       try {
+//         page.on("response", async (resp) => {
+//           const request = resp.request();
+//           if (request.url().includes("sal-service/v1/fund/process/stockStyle")) {
+//             const stringText = await resp.text();
+//             const text = JSON.parse(stringText);
+//             const stock = {
+//               [funds[i]]: {
+//                     "Price/Earnings": {
+//                       Fund: text.fund.prospectiveEarningsYield.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.prospectiveEarningsYield.toFixed(2),
+//                       Index: text.indexAverage.prospectiveEarningsYield.toFixed(2),
+//                     },
+//                     "Price/Book": {
+//                       Fund: text.fund.prospectiveBookValueYield.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.prospectiveBookValueYield.toFixed(2),
+//                       Index: text.indexAverage.prospectiveBookValueYield.toFixed(2),
+//                     },
+//                     "Price/Sales": {
+//                       Fund: text.fund.prospectiveRevenueYield.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.prospectiveRevenueYield.toFixed(2),
+//                       Index: text.indexAverage.prospectiveRevenueYield.toFixed(2),
+//                     },
+//                     "Price/Cash Flow": {
+//                       Fund: text.fund.prospectiveCashFlowYield.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.prospectiveCashFlowYield.toFixed(2),
+//                       Index: text.indexAverage.prospectiveCashFlowYield.toFixed(2),
+//                     },
+//                     "Dividend Yield %'": {
+//                       Fund: text.fund.prospectiveDividendYield.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.prospectiveDividendYield.toFixed(2),
+//                       Index: text.indexAverage.prospectiveDividendYield.toFixed(2),
+//                     },
+//                     "Long-Term Earnings %": {
+//                       Fund: text.fund.forecasted5YearEarningsGrowth.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.forecasted5YearEarningsGrowth.toFixed(2),
+//                       Index: text.indexAverage.forecasted5YearEarningsGrowth.toFixed(2),
+//                     },
+//                     "Historical Earnings %": {
+//                       Fund: text.fund.forecastedEarningsGrowth.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.forecastedEarningsGrowth.toFixed(2),
+//                       Index: text.indexAverage.forecastedEarningsGrowth.toFixed(2),
+//                     },
+//                     "Sales Growth %": {
+//                       Fund: text.fund.forecastedRevenueGrowth.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.forecastedRevenueGrowth.toFixed(2),
+//                       Index: text.indexAverage.forecastedRevenueGrowth.toFixed(2),
+//                     },
+//                     "Cash-Flow Growth %": {
+//                       Fund: text.fund.forecastedCashFlowGrowth.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.forecastedCashFlowGrowth.toFixed(2),
+//                       Index: text.indexAverage.forecastedCashFlowGrowth.toFixed(2),
+//                     },
+//                     "Book-Value Growth %": {
+//                       Fund: text.fund.forecastedBookValueGrowth.toFixed(2),
+//                       "Cat. Average": text.categoryAverage.forecastedBookValueGrowth.toFixed(2),
+//                       Index: text.indexAverage.forecastedBookValueGrowth.toFixed(2),
+//                     },
+//                   }
+//               };
+//               stockTable.push(stock);
+//           }
+//         });
+//         await page.goto(
+//           `https://www.morningstar.com/funds/xnas/${funds[i]}/portfolio`
+//         );
+//         await page.waitForTimeout(2500);
+//         await browser.close();
+//       } catch {
+//         console.log(`Error getting ${funds[i]}`);
+//         await browser.close();
+//       }
+//     }
+//     console.log(stockTable);
+//     return stockTable;
+// }
+
+// testPortfolio(stocks);
--- ng-bev-server/Puppeteer/risk.js
+++ ng-bev-server/Puppeteer/risk.js
@@ -1,113 +1,122 @@
 const puppeteer = require('puppeteer');
 
-const stocks = [
-  'etilx',
-  'etihx',
-  'etimx',
-  'etidx',
-  'etibx',
-  'pksfx',
-  'pxsgx',
-  'viisx',
-  'ggezx',
-  'ambfx',
-  'amefx',
-  'gvtfx',
-]
-
-const generateRiskTable = async (funds) => {
-  const stockTable = [];
+exports.methods = {
+  generateRiskTable: async (funds) => {
+    const stockTable = [];
 
   for (let i = 0; i < funds.length; i++) {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
     try {
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
+      page.on("response", async(resp) => {
+        const request = resp.request();
+        if (request.url().includes("sal-service/v1/fund/performance/riskVolatility")) {
+          const stringText = await resp.text();
+          const text = JSON.parse(stringText);
+          const stock = {
+            [funds[i]]: {
+              "Alpha": {
+                "Fund": text.fundRiskVolatility.for3Year.alpha === null ? null : text.fundRiskVolatility.for3Year.alpha.toFixed(2),
+                "Category": text.categoryRiskVolatility.for3Year.alpha === null ? null : text.categoryRiskVolatility.for3Year.alpha.toFixed(2),
+                "Index": text.indexRiskVolatility.for3Year.alpha === null ? null : text.indexRiskVolatility.for3Year.alpha.toFixed(2),
+              },
+              "Beta": {
+                "Fund": text.fundRiskVolatility.for3Year.beta === null ? null : text.fundRiskVolatility.for3Year.beta.toFixed(2),
+                "Category": text.categoryRiskVolatility.for3Year.beta === null ? null : text.categoryRiskVolatility.for3Year.beta.toFixed(2),
+                "Index": text.indexRiskVolatility.for3Year.beta === null ? null : text.indexRiskVolatility.for3Year.beta.toFixed(2),
+              },
+              "RSquared": {
+                "Fund": text.fundRiskVolatility.for3Year.rSquared === null ? null : text.fundRiskVolatility.for3Year.rSquared.toFixed(2),
+                "Category": text.categoryRiskVolatility.for3Year.rSquared === null ? null : text.categoryRiskVolatility.for3Year.rSquared.toFixed(2),
+                "Index": text.indexRiskVolatility.for3Year.rSquared === null ? null : text.indexRiskVolatility.for3Year.rSquared.toFixed(2),
+              },
+              "Sharpe Ratio": {
+                "Fund": text.fundRiskVolatility.for3Year.sharpeRatio === null ? null : text.fundRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+                "Category": text.categoryRiskVolatility.for3Year.sharpeRatio === null ? null : text.categoryRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+                "Index": text.indexRiskVolatility.for3Year.sharpeRatio === null ? null : text.indexRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+              },
+              "Standard Deviation": {
+                "Fund": text.fundRiskVolatility.for3Year.standardDeviation === null ? null : text.fundRiskVolatility.for3Year.standardDeviation.toFixed(2),
+                "Category": text.categoryRiskVolatility.for3Year.standardDeviation === null ? null : text.categoryRiskVolatility.for3Year.standardDeviation.toFixed(2),
+                "Index": text.indexRiskVolatility.for3Year.standardDeviation === null ? null : text.indexRiskVolatility.for3Year.standardDeviation.toFixed(2),
+              },
+            },
+          };
+          stockTable.push(stock);
+        }
+      });
       await page.goto(
         `https://www.morningstar.com/funds/xnas/${funds[i]}/risk`
       );
-
-      await page.waitFor(4000);
-
-      if (
-        (await page.$(
-          "#__layout > div > div:nth-child(6) > div > div > header > div > div.mds-masthead__right > button"
-        )) !== null
-      ) {
-        await page.click(
-          "#__layout > div > div:nth-child(6) > div > div > header > div > div.mds-masthead__right > button"
-        );
-        console.log("clicked");
-      }
-
-      if (
-        (await page.$(
-          "#__layout > div > div:nth-child(6) > div > div > div.mdc-overlay__button-container > button"
-        )) !== null
-      ) {
-        await page.click(
-          "#__layout > div > div:nth-child(6) > div > div > div.mdc-overlay__button-container > button"
-        );
-        console.log("clicked");
-      }
-
-      await page.waitForSelector(
-        "#__layout > div > div.mdc-page-shell__content.mds-page-shell__content > main > div.fund__content > div > div > div.mdc-column.mds-layout-grid__col.fund__content-sal.mds-layout-grid__col--12.mds-layout-grid__col--auto-at-1092 > sal-components > section > div > div > div > sal-components-mip-risk > div > div:nth-child(2) > div > div.sal-component-body > div:nth-child(1) > div:nth-child(2) > div > sal-components-mip-risk-volatility-measures > div > div:nth-child(3) > div > div.sal-risk-volatility-measures__dataTable"
-      );
-
-      const table = await page.$$eval(
-        "#__layout > div > div.mdc-page-shell__content.mds-page-shell__content > main > div.fund__content > div > div > div.mdc-column.mds-layout-grid__col.fund__content-sal.mds-layout-grid__col--12.mds-layout-grid__col--auto-at-1092 > sal-components > section > div > div > div > sal-components-mip-risk > div > div:nth-child(2) > div > div.sal-component-body > div:nth-child(1) > div:nth-child(2) > div > sal-components-mip-risk-volatility-measures > div > div:nth-child(3) > div > div.sal-risk-volatility-measures__dataTable > table > tbody > tr",
-        (rows) => rows.map((row) => row.innerText)
-      );
-
-      const spaced = table.map((row) => row.split("\t"));
-
-      const stock = {
-        [funds[i]]: {
-          "Alpha": {
-            Fund: spaced[0][1],
-            Category: spaced[0][2],
-            Index: spaced[0][3],
-          },
-          "Beta": {
-            Fund: spaced[1][1],
-            Category: spaced[1][2],
-            Index: spaced[1][3],
-          },
-          "RSquared": {
-            Fund: spaced[2][1],
-            Category: spaced[2][2],
-            Index: spaced[2][3],
-          },
-          "Sharpe Ratio": {
-            Fund: spaced[3][1],
-            Category: spaced[3][2],
-            Index: spaced[3][3],
-          },
-          "Standard Deviation": {
-            Fund: spaced[4][1],
-            Category: spaced[4][2],
-            Index: spaced[4][3],
-          },
-        },
-      };
-
+      await page.waitForTimeout(2500);
       await browser.close();
-      console.log(stock);
-      stockTable.push(stock);
     } catch {
       console.log(`Error getting ${funds[i]}`);
+      await browser.close();
     }
   }
-
-  console.log(stockTable);
+  return stockTable;
+  },
 };
 
-generateRiskTable(stocks);
+// testRisk = async funds => {
+//   const stockTable = [];
 
-// const stuff = stocks.map(stock => generateRiskTable(stock));
+//   for (let i = 0; i < funds.length; i++) {
+//     const browser = await puppeteer.launch({headless: false});
+//     const page = await browser.newPage();
+//     try {
+//       page.on("response", async(resp) => {
+//         const request = resp.request();
+//         if (request.url().includes("sal-service/v1/fund/performance/riskVolatility")) {
+//           const stringText = await resp.text();
+//           const text = JSON.parse(stringText);
+//           const stock = {
+//             [funds[i]]: {
+//               "Alpha": {
+//                 Fund: text.fundRiskVolatility.for3Year.alpha === null ? null : text.fundRiskVolatility.for3Year.alpha.toFixed(2),
+//                 Category: text.categoryRiskVolatility.for3Year.alpha === null ? null : text.categoryRiskVolatility.for3Year.alpha.toFixed(2),
+//                 Index: text.indexRiskVolatility.for3Year.alpha === null ? null : text.indexRiskVolatility.for3Year.alpha.toFixed(2),
+//               },
+//               "Beta": {
+//                 Fund: text.fundRiskVolatility.for3Year.beta === null ? null : text.fundRiskVolatility.for3Year.beta.toFixed(2),
+//                 Category: text.categoryRiskVolatility.for3Year.beta === null ? null : text.categoryRiskVolatility.for3Year.beta.toFixed(2),
+//                 Index: text.indexRiskVolatility.for3Year.beta === null ? null : text.indexRiskVolatility.for3Year.beta.toFixed(2),
+//               },
+//               "RSquared": {
+//                 Fund: text.fundRiskVolatility.for3Year.rSquared === null ? null : text.fundRiskVolatility.for3Year.rSquared.toFixed(2),
+//                 Category: text.categoryRiskVolatility.for3Year.rSquared === null ? null : text.categoryRiskVolatility.for3Year.rSquared.toFixed(2),
+//                 Index: text.indexRiskVolatility.for3Year.rSquared === null ? null : text.indexRiskVolatility.for3Year.rSquared.toFixed(2),
+//               },
+//               "Sharpe Ratio": {
+//                 Fund: text.fundRiskVolatility.for3Year.sharpeRatio === null ? null : text.fundRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+//                 Category: text.categoryRiskVolatility.for3Year.sharpeRatio === null ? null : text.categoryRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+//                 Index: text.indexRiskVolatility.for3Year.sharpeRatio === null ? null : text.indexRiskVolatility.for3Year.sharpeRatio.toFixed(2),
+//               },
+//               "Standard Deviation": {
+//                 Fund: text.fundRiskVolatility.for3Year.standardDeviation === null ? null : text.fundRiskVolatility.for3Year.standardDeviation.toFixed(2),
+//                 Category: text.categoryRiskVolatility.for3Year.standardDeviation === null ? null : text.categoryRiskVolatility.for3Year.standardDeviation.toFixed(2),
+//                 Index: text.indexRiskVolatility.for3Year.standardDeviation === null ? null : text.indexRiskVolatility.for3Year.standardDeviation.toFixed(2),
+//               },
+//             },
+//           };
+//           console.log(stock);
+//           stockTable.push(stock);
+//         }
+//       });
+//       await page.goto(
+//         `https://www.morningstar.com/funds/xnas/${funds[i]}/risk`
+//       );
+//       await page.waitForTimeout(2500);
+//       await browser.close();
+//     } catch {
+//       console.log(`128 Error getting ${funds[i]}`);
+//       await browser.close();
+//     }
+//   }
+//   console.log(stockTable);
+//   console.log(stockTable.length);
+//   return stockTable;
+// };
 
-// Promise.all(stuff).then(values => console.log(values));
-
-const tables = [];
-
-
+// testRisk(stocks);
